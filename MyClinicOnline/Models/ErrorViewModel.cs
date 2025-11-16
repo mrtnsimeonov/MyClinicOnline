36eae81e93b6c7706cec0bf51e8f79eb11b334b9
mrtnsimeonov <martin.simeonov.highschool@buditel.bg>
2025-11-16 14:24:59 +0200

    Add project files.

---
+++ MyClinicOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyClinicOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

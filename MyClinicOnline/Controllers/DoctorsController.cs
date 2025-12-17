0a06c83ee4a362471f77ce915b3f5def114ec8de
mrtnsimeonov <martin.simeonov.highschool@buditel.bg>
2025-12-18 00:48:22 +0200

    SearchDoctorsBySpecialty

---
+++ MyClinicOnline/Controllers/DoctorsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyClinicOnline.Data;
+
+namespace MyClinicOnline.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private readonly MyClinicOnlineContext _context;
+
+        public DoctorsController(MyClinicOnlineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Doctors/Search?specialty=Cardiology
+        public IActionResult Search(string specialty)
+        {
+            if (string.IsNullOrWhiteSpace(specialty))
+            {
+                return View(new List<Models.Doctor>());
+            }
+
+            var doctors = _context.Doctors
+                .Include(d => d.Specialties)
+                    .ThenInclude(ds => ds.Specialty)
+                .Where(d =>
+                    d.Specialties.Any(ds =>
+                        ds.Specialty.Name.Contains(specialty)))
+                .ToList();
+
+            ViewBag.Specialty = specialty;
+            return View(doctors);
+        }
+    }
+}

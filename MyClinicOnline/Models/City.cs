18f99e17d7739ecbbec01bcd6e27730c03e56572
mrtnsimeonov <martin.simeonov.highschool@buditel.bg>
2026-01-02 16:21:15 +0200

    updated models

---
+++ MyClinicOnline/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace MyClinicOnline.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public List<Doctor> Doctors { get; set; } = new();
+    }
+}
--- MyClinicOnline/Models/Doctor.cs
+++ MyClinicOnline/Models/Doctor.cs
@@ -11,6 +11,10 @@ namespace MyClinicOnline.Models
 
         public bool WorksWithNhif { get; set; }
 
+        // ✅ NEW: City relation
+        public int CityId { get; set; }
+        public City City { get; set; } = null!;
+
         public List<DoctorSpecialty> Specialties { get; set; } = new();
         public List<TimeSlot> TimeSlots { get; set; } = new();
         public List<Appointment> Appointments { get; set; } = new();

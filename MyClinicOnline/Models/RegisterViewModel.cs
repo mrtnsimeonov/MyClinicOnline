cfea5310c27f5197eaf190667aa3890447217b17
mrtnsimeonov <martin.simeonov.highschool@buditel.bg>
2026-01-18 18:42:09 +0200

    RegisterViewModel

---
+++ MyClinicOnline/Models/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyClinicOnline.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Името е задължително")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Фамилията е задължителна")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Телефонът е задължителен")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Изберете област")]
+        public string Region { get; set; }
+
+        [Required(ErrorMessage = "Изберете пол")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Датата на раждане е задължителна")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Имейлът е задължителен")]
+        [EmailAddress(ErrorMessage = "Невалиден имейл")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Паролата е задължителна")]
+        [MinLength(8, ErrorMessage = "Паролата трябва да е поне 8 символа")]
+        public string Password { get; set; }
+    }
+}
